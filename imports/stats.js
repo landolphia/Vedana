810e1d6cc90622dfc1ba15922a55e8231fb1c402
landolphia <grimskunk@gmail.com>
2017-10-29 16:25:18 -0400

    implemented basic stats

--- imports/stats.js
+++ imports/stats.js
@@ -15,18 +15,44 @@ if (Meteor.isServer) {
 
 			let jobs = Jobs.find({});
 			let expenses = Expenses.find({});
+
+			let totalEarnings = 0;
+			let goodMood = 0;
+			jobs.forEach((job) => {
+				totalEarnings += job.amount;
+				if (job.mood) goodMood++;
+			});
+
+			Stats.insert({ label: "Total earned", amount: totalEarnings});
+			Stats.insert({ label: "Total # of jobs", amount: jobs.count()});
+			Stats.insert({ label: "Ratio of mood (between 0 and 1)", amount: (goodMood / jobs.count()).toFixed(2)});
+
+			let totalExpenses = 0;
+			expenses.forEach((expense) => {
+				totalExpenses += expense.amount;
+			});
+			Stats.insert({ label: "Total spent", amount: totalExpenses});
+
+			let income = (totalEarnings - totalExpenses).toFixed(2);
+
+			if (income < 0) {
+				Stats.insert({ label: "Catchup", amount: -income});
+				Stats.insert({ label: "Income", amount: 0});
+			} else {
+				Stats.insert({ label: "Catchup", amount: 0});
+				Stats.insert({ label: "Net income", amount: income});
+			}
+
+
+			//TODO everything below
 			Stats.insert({ label: "Stats date range", amount: "TODO (7 days from Monday to Sunday, will use getRange from date utility.)"});
 
-			Stats.insert({ label: "Total earned", amount: "TODO"});
-			Stats.insert({ label: "Total earned before today", amount: "TODO"});
 			Stats.insert({ label: "Today's earnings", amount: "TODO"});
-			Stats.insert({ label: "Total spent", amount: "TODO"});
+			Stats.insert({ label: "Total earned before today", amount: "TODO"});
 
-			Stats.insert({ label: "Number of jobs (weekly/daily)", amount: "TODO"});
 
 			Stats.insert({ label: "Average earnings (including/excluding today)", amount: "TODO"});
 			Stats.insert({ label: "Projections (left to reach goal, bonus if go on similarly, etc)", amount: "TODO"});
-			Stats.insert({ label: "Ratio of mood (between 0 and 1)", amount: "TODO"});
 
 
 			Stats.insert({ label: "Open days (passed/left/total)", amount: "TODO"});
@@ -34,22 +60,6 @@ if (Meteor.isServer) {
 
 			Stats.insert({ label: "Week max daily amount", amount: "TODO"});
 			Stats.insert({ label: "Week min daily amount", amount: "TODO (>0)"});
-
-			Stats.insert({ label: "Needed for week", amount: "spent-earned IF > 0"});
-			Stats.insert({ label: "Bonus for week", amount: "earned-spent IF > 0"});
-
-			//	Meteor.call('jobs.total', {}, (err, res) => {
-			//		if (err) {
-			//			console.log("Error retrieving wages. " + err);
-			//		} else {
-			//			template.stats.set('jobsTotal', res);
-			//			return res;
-			//		}
-
-			//	});
-			//} else {
-			//	return total;
-			//}
 		}
 
 	});
@@ -58,9 +68,3 @@ if (Meteor.isServer) {
 		return Stats.find();
 	});
 }
-
-Meteor.methods({
-	'stats'(){
-		return Stats.find({});
-	},
-});
