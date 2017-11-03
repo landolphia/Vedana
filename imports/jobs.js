97ac58c96b856dbc8b738cf15224cbe24ba452c2
Landolphia <landolphia@users.noreply.github.com>
2017-11-02 20:56:35 -0400

    Updated test data and roadmap

--- imports/jobs.js
+++ imports/jobs.js
@@ -9,22 +9,39 @@ if (Meteor.isServer) {
 		Jobs.remove({});
 		if (Jobs.find().count() === 0) {
 			console.log("Populating DB with jobs.");
-			Jobs.insert({ amount: 19.00, mood: true});
-			Jobs.insert({ amount: 15.67, mood: true});
-			Jobs.insert({ amount: 10.03, mood: true});
-			Jobs.insert({ amount: 37.21, mood: true});
-			Jobs.insert({ amount: 5.39, mood: true});
-			Jobs.insert({ amount: 6.92, mood: true});
-			Jobs.insert({ amount: 4.68, mood: true});
-			Jobs.insert({ amount: 39.88, mood: true});
-			Jobs.insert({ amount: 1.12, mood: true});
-			Jobs.insert({ amount: 4.53, mood: true});
-			Jobs.insert({ amount: 8.97, mood: true});
-			Jobs.insert({ amount: 1.15, mood: true});
-			Jobs.insert({ amount: 17.00, mood: true});
-			Jobs.insert({ amount: 1.74, mood: true});
-			Jobs.insert({ amount: 9.92, mood: true});
-			Jobs.insert({ amount: 73.32, mood: false});
+			//Jobs.insert({ amount: 19.00, mood: true});
+			//Jobs.insert({ amount: 15.67, mood: true});
+			//Jobs.insert({ amount: 10.03, mood: true});
+			//Jobs.insert({ amount: 37.21, mood: true});
+			//Jobs.insert({ amount: 5.39, mood: true});
+			//Jobs.insert({ amount: 6.92, mood: true});
+			//Jobs.insert({ amount: 4.68, mood: true});
+			//Jobs.insert({ amount: 39.88, mood: true});
+			//Jobs.insert({ amount: 1.12, mood: true});
+			//Jobs.insert({ amount: 4.53, mood: true});
+			//Jobs.insert({ amount: 8.97, mood: true});
+			//Jobs.insert({ amount: 1.15, mood: true});
+			//Jobs.insert({ amount: 17.00, mood: true});
+			//Jobs.insert({ amount: 1.74, mood: true});
+			//Jobs.insert({ amount: 9.92, mood: true});
+			//Jobs.insert({ amount: 73.32, mood: false});
+			
+			// 30/10
+			Jobs.insert({ amount: 35.80, mood: true});
+			// 31/10
+			Jobs.insert({ amount: 33.66, mood: true});
+			Jobs.insert({ amount: 24.76, mood: true});
+			Jobs.insert({ amount: 1.88, mood: true});
+			Jobs.insert({ amount: 23.27, mood: true});
+			// 1/11
+			Jobs.insert({ amount: 28.29, mood: true});
+			// 2/11
+			Jobs.insert({ amount: 32.44, mood: true});
+			Jobs.insert({ amount: 0.45, mood: true});
+			Jobs.insert({ amount: 18.62, mood: true});
+			Jobs.insert({ amount: 1.14, mood: true});
+			Jobs.insert({ amount: 2.52, mood: true});
+			Jobs.insert({ amount: 8.84, mood: true});
 		}
 
 	});
