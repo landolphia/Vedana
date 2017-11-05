b0c3045a46ff7ac5395e59995cc9c494c9591172
Landolphia <landolphia@users.noreply.github.com>
2017-11-05 13:21:43 -0500

    Scaffolding for date, including week range 4 mongo

--- server/main.js
+++ server/main.js
@@ -1,3 +1,14 @@
 import '../imports/expenses.js';
 import '../imports/jobs.js';
 import '../imports/stats.js';
+
+import '../imports/dates.js';
+let now = new Date();
+Meteor.call('getToday', now);
+Meteor.call('getWeekRange', now, function (err, res) {
+	if (err) {
+		console.log("Error retrieving week range.");
+	} else {
+		console.log("Week range = " + JSON.stringify(res));
+	};
+});
